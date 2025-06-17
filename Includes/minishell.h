abdb5f89da21e87b648ab9453dad902fc76a3caf
erantala <erantala@student.hive.fi>
2025-06-17 16:06:48 +0300

    Initial commit

---
+++ Includes/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erantala <erantala@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/17 16:05:32 by erantala          #+#    #+#             */
+/*   Updated: 2025/06/17 16:06:23 by erantala         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+
+#endif
