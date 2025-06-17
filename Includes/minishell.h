c18cc596a4784a662ec7da6740bae0197b7ee1e6
erantala <erantala@student.hive.fi>
2025-06-17 17:15:31 +0300

    Added intitial memory arena logic.

--- Includes/minishell.h
+++ Includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: erantala <erantala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 16:05:32 by erantala          #+#    #+#             */
-/*   Updated: 2025/06/17 16:06:23 by erantala         ###   ########.fr       */
+/*   Updated: 2025/06/17 16:56:03 by erantala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,17 @@
 #include <signal.h>
 #include <stdlib.h>
 
+# define ARENA_SIZE 16384
+# define ALIGNMENT 8
+
+typedef struct s_arena
+{
+	size_t	size;
+	size_t	max;
+	size_t	index;
+	char	data[];
+}	t_arena;
+
+void	*arena_malloc(size_t n);
 
 #endif
--- srcs/memory_arena.c
+++ srcs/memory_arena.c
@@ -6,8 +6,83 @@
 /*   By: erantala <erantala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 16:05:04 by erantala          #+#    #+#             */
-/*   Updated: 2025/06/17 16:05:17 by erantala         ###   ########.fr       */
+/*   Updated: 2025/06/17 17:15:26 by erantala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../Includes/minishell.h"
+
 // Memory arena to store all allocated memory in one location.
+
+void	*arena_malloc(size_t n)
+{
+	t_arena	*arena;
+	void	*ret;
+
+	arena = get_arenas(n);
+	arena->size += n;
+	ret = &arena->data[arena->index];
+	arena->index += n + 1;
+	return (ret);
+}
+
+// gets the next available arena and returns a pointer to the free memory
+
+t_arena *init_arena(size_t size)
+{
+	t_arena	*arena;
+
+	arena = malloc(size);
+	if (!arena)
+		exit(1);
+	arena->max = size;
+	arena->size = 0;
+	arena->index = 0;
+	return (arena);
+}
+
+// Returns a free point in the arena, if arenas are full allocates more.
+
+t_arena *get_arenas(size_t n)
+{
+	static int		arena_count = 0;
+	static int		index = 0;
+	static t_arena **arenas;
+
+	if (arena_count < 2)
+	{
+		arenas = malloc(sizeof(t_arena *) * 2);
+		if (!arenas)
+			exit;
+		arenas[arena_count++] = init_arena(ARENA_SIZE);
+		arenas[arena_count++] = init_arena(ARENA_SIZE);
+	}
+	while (index < arena_count && arenas[index]->max - arenas[index]->size < n)
+	while (arenas[index]->max - arenas[index]->size < n && index < arena_count)
+		index++;
+	if (index == arena_count)
+	{
+		arenas = new_arena(arenas, arena_count, n);
+		index++;
+	}
+	return (arenas[index]);
+}
+
+// Creates a new arena when there isn't enough memory in current arenas.
+
+t_arena **new_arena(t_arena **curr, int	count, size_t n)
+{
+	t_arena	**arenas;
+	int		i = 0;
+
+	arenas = malloc(sizeof(t_arena *) * count + 1);
+	if (!arenas)
+		exit(1);
+	while (i < count)
+	{
+		arenas[i] = curr[i];
+		i++;
+	}
+	arenas[i] = init_arena(n);
+	return (arenas);
+}
