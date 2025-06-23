7a637397c770768c5a34609fe549dfaa8608499c
Julius Pellinen <jpelline@c1r3p6.hive.fi>
2025-06-23 15:54:39 +0300

    added t_data struct to .h file for heredoc

--- include/minishell.h
+++ include/minishell.h
@@ -13,12 +13,18 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdio.h>
-# include <signal.h>
-# include <stdlib.h>
 # include "libft.h"
-# include <readline/readline.h>
+# include <assert.h>
+# include <errno.h>
+# include <fcntl.h>
 # include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/wait.h>
 
 # define ARENA_SIZE 16384
 # define ALIGNMENT 8
@@ -28,14 +34,13 @@ typedef struct s_arena
 	size_t	max;
 	size_t	index;
 	char	data[];
-}	t_arena;
+}			t_arena;
 
-// typedef	struct s_data
-// {
-// 	/* data */
-// }	t_data;
-// MAKE A STRUCT TO STORE ALL DATA FDS, COMMANDS ETC.s
-
+typedef	struct s_data
+{
+	int	*hdfd;
+	int	hd_count;
+}	t_data;
 
 typedef enum e_type
 {
@@ -45,60 +50,65 @@ typedef enum e_type
 	OUTPUT,
 	APPEND,
 	HERE_DOC,
+<<<<<<< HEAD
 	STRING,
 	FILE
 }	t_type;
+=======
+	STRING
+}			t_type;
+>>>>>>> 8edfda1 (added t_data struct to .h file for heredoc)
 
-typedef	struct s_token
+typedef struct s_token
 {
 	t_type	t;
 	char	*s;
-}	t_token;
+}			t_token;
 
-typedef	struct s_vector
+typedef struct s_vector
 {
 	size_t	count;
 	size_t	size;
 	void	**data;
-}	t_vector;
+	t_data	datapool;
+}			t_vector;
 
-typedef	struct s_variable
+typedef struct s_variable
 {
 	char	*key;
 	char	*expansion;
-}	t_var;
+}			t_var;
 
 typedef struct s_command
 {
 	char	*str;
 	t_type	type;
 	t_type	next;
-} t_cmd;
+}			t_cmd;
 
-
-t_arena 	*init_arena(size_t size);
+t_arena		*init_arena(size_t size);
 void		*arena_malloc(size_t n);
-t_arena 	**get_arenas(void);
-t_arena 	*find_arena(size_t n);
-t_arena		**new_arena(t_arena **curr, int	count, size_t n);
+t_arena		**get_arenas(void);
+t_arena		*find_arena(size_t n);
+t_arena		**new_arena(t_arena **curr, int count, size_t n);
 
 t_vector	*new_vector(size_t elem);
 void		expand_vector(t_vector **vector, size_t elems);
 int			add_elem(t_vector *vector, void *elem);
 void		change_data(t_vector *vector, void *elem, void *target);
 
-char		*take_input();
+char		*take_input(void);
 int			check_quotes(char *s);
 
 t_token		*create_token(char *s, size_t *i);
 t_vector	*token_vector(char *s);
-char		*token_string(char	*s, size_t	*i);
+char		*token_string(char *s, size_t *i);
 t_vector	*create_commands(t_vector *tokens);
 t_cmd		*make_cmd_spc(t_vector *tokens, size_t *i);
 t_cmd		*make_cmd_str(t_vector *tokens, size_t *i);
 size_t		expanded_length(char *s, size_t n);
 
-int			check_specials(int	c);
+int			check_specials(int c);
 char		*expand_strndup(char *s, size_t n);
 char		*mini_append(char *s1, char *s2);
 
@@ -108,6 +118,9 @@ size_t		word_len(char *s);
 
 char		*find_export(char *key);
 void		export(char *key, char *expansion);
-t_vector	*get_vars();
+t_vector	*get_vars(void);
+
+void	here_doc(t_vector *tokens, char *limiter, int index);
+
 
 #endif
