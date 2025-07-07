41baad0d986ccd4593d3e5c8706486a39d17e3cb
erantala <erantala@student.hive.fi>
2025-07-07 20:26:57 +0300

    Reformatted functions header

--- include/functions.h
+++ include/functions.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   functions.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpelline <jpelline@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: erantala <erantala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 19:21:07 by jpelline          #+#    #+#             */
-/*   Updated: 2025/07/07 19:21:08 by jpelline         ###   ########.fr       */
+/*   Updated: 2025/07/07 20:26:43 by erantala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,71 @@
 
 # include "structs.h"
 
-void		here_two(t_vector *tokens, int count, t_data *data);
-int		here_count(t_vector *tokens, t_data *data);
+t_vector	*next_check(t_vector *commands);
 
-void		init_data(char **env);
-char		*get_pwd(void);
-size_t		ft_stralen(char **s);
-char		*mini_substr(char const *s, unsigned int start, size_t len);
-char		**mini_split(const char *s, char c);
-char		*mini_itoa(int n);
-char		*ft_strornchr(const char *s, int one, int two, size_t n);
+// Tokenizer
 
-// Helpers
+t_token		*create_token(char *s, size_t *i, t_type last, t_data *data);
+t_vector	*token_vector(char *s);
+char		*token_string(char *s, size_t *i, t_type last);
+t_vector	*creator(char *s, size_t len, size_t i, t_data *data);
+char		*quoted_token(char *s, char quote, size_t *i, t_type last);
+
+// Command creator
+
+t_vector	*create_commands(t_vector *tokens);
+void		cmd_help(t_vector *tks, size_t *i, t_token *tk, t_cmd *cm);
+t_cmd		*make_cmd_spc(t_vector *tokens, size_t *i, t_data *data);
+t_cmd		*make_cmd_str(t_vector *tokens, size_t *i, t_data *data);
+
+// Input
+
+void		non_interactive(void);
+char		*get_input(char **argv, int argc);
+char		*take_input(void);
+int			check_quotes(char *s);
+
+// Syntax Check
+
+t_vector	*next_check(t_vector *commands);
+void		check_repeat(t_vector *tokens);
+void		check_command_syntax(t_vector *commands, t_data *data);
+void		syntax_print(char *error, t_data *data, int exit, bool per);
+char		*ambigous(char *s, size_t i);
+
+// Built In
+
 void		init_export(void);
 void		sort_export(size_t count, t_data *data, char **exps);
 void		empty_export(void);
-char		**strarm(char **s, size_t i);
+void		echo(t_cmd **cmd, int i);
+void		pwd(void);
+void		unset(char *key);
+void		cd(t_cmd **cmd, int i);
+void		env(void);
+void		export(char *export);
 
-// Export sort
-void		cmd_help(t_vector *tokens, size_t *i, t_token *token, t_cmd *cmd);
+char		**export_to_arr(char *key, char **exports);
+char		**unset_child(char *key, char **envi);
+char		*find_export(char *key);
+void		make_export(char *command);
+void		replace_export(char *key);
+void		child_builds(t_cmd **cmds, char **envi, int i);
+void		built_in(t_cmd *cmd);
+void		build_handler(t_cmd **cmds);
+char		*expans_help(char *s, char *dup, size_t *i, size_t *pos);
+
+// Memory Arena
+
 t_arena		*init_arena(size_t size);
 void		*arena_malloc(size_t n);
 t_arena		**get_arenas(t_arena **new);
 t_arena		*find_arena(size_t n);
 t_arena		**new_arena(t_arena **curr, int count, size_t n);
-void		child_died(int status);
 
-// Memory arena
+
+// Vector
+
 t_vector	*new_vector(size_t elem);
 void		expand_vector(t_vector *vector);
 void		add_elem(t_vector *vector, void *elem);
@@ -50,85 +88,56 @@ void		array_to_vec(t_vector *vec, void **arr);
 char		**vec_to_array(t_vector *vec);
 void		remove_elem(t_vector *vector, size_t i);
 
-// Vectors
-char		*take_input(void);
-int		check_quotes(char *s);
+// Helpers
 
-// Input
-t_token		*create_token(char *s, size_t *i, t_type last, t_data *data);
-t_vector	*token_vector(char *s);
-char		*token_string(char *s, size_t *i, t_type last);
-t_vector	*creator(char *s, size_t len, size_t i, t_data *data);
-
-// Token creator
-t_vector	*create_commands(t_vector *tokens);
-void		cmd_help(t_vector *tks, size_t *i, t_token *tk, t_cmd *cm);
-t_cmd		*make_cmd_spc(t_vector *tokens, size_t *i, t_data *data);
-t_cmd		*make_cmd_str(t_vector *tokens, size_t *i, t_data *data);
-
-// Command creator
-size_t		expanded_length(char *s, size_t n);
-
-int			check_specials(int c, int quote);
-char		*expand_strndup(char *s, size_t n);
-char		*expand_strndup(char *s, size_t n);
-char		*mini_append(char *s1, char *s2);
-
-char		*quoted_token(char *s, char quote, size_t *i, t_type last);
-char		*mini_strndup(char *s, size_t n);
-char		*mini_strdup(char *s);
-size_t		word_len(char *s, int quote);
-t_vector	*next_check(t_vector *commands);
-
-void		check_repeat(t_vector *tokens);
-size_t		quote_len(char *s, char quote);
-void		check_command_syntax(t_vector *commands, t_data *data);
-
-// Parsing
-void		syntax_print(char *error, t_data *data, int exit, bool per);
-
-// Syntax
+void		init_data(char **env);
+char		*get_pwd(void);
+void		increase_shell_lvl(void);
 t_data		*get_data(void);
+void		clean_heredoc(void);
+void		ft_exit_child(char *s, int code);
 void		free_arenas(void);
 void		ft_exit(char *s, int code);
+int			exit_calci(char *cmd);
+
+// Utility
+
+size_t		expanded_length(char *s, size_t n);
+int			check_specials(int c, int quote);
+int			find_line(char **line, char *key);
+size_t		quote_len(char *s, char quote);
+char		**ft_stradd(char **s, char *line);
+char		**strarm(char **s, size_t i);
+char		**replace_line(char **s, char *line, size_t i);
+size_t		key_len(char *s);
+size_t		word_len(char *s, int quote);
+
+// String functions
+
+char		**ft_strremove(char **s, int i);
+size_t		ft_stralen(char **s);
+char		*mini_substr(char const *s, unsigned int start, size_t len);
+char		**mini_split(const char *s, char c);
+char		*mini_itoa(int n);
+char		*expand_strndup(char *s, size_t n);
+char		*mini_append(char *s1, char *s2);
+char		*mini_join(char const *s1, char const *s2);
+char		*expand_strndup(char *s, size_t n);
+char		*ft_strornchr(const char *s, int one, int two, size_t n);
 
 // Heredoc
+
 void		here_check(int fd, char *name, t_data *data, size_t i);
 char		*here_expansion(char *ln, size_t *i);
 int			check_heredoc(t_vector *tokens);
+void		fix_lines(char **file, size_t i, char *name, t_data *data);
 char		*here_doc(char *limiter, int index, t_type type);
 char		*name_join(char const *s1, char const *s2);
-void		fix_lines(char **file, size_t i, char *name, t_data *data);
+void		here_two(t_vector *tokens, int count, t_data *data);
+int			here_count(t_vector *tokens, t_data *data);
 
-// Built Ins
-void		echo(t_cmd **cmd, int i);
-void		pwd(void);
-void		unset(char *key);
-void		cd(t_cmd **cmd, int i);
-void		env(void);
-void		export(char *export);
-char		**export_to_arr(char *key, char **exports);
-char		**unset_child(char *key, char **envi);
-char		*find_export(char *key);
-void		make_export(char *command);
-void		replace_export(char *key);
+// Signal handling
 
-// Built In helpers
-
-void		child_builds(t_cmd **cmds, char **envi, int i);
-void		built_in(t_cmd *cmd);
-void		build_handler(t_cmd **cmds);
-char		*expans_help(char *s, char *dup, size_t *i, size_t *pos);
-void		increase_shell_lvl(void);
-int		exit_calci(char *cmd);
-char		*mini_join(char const *s1, char const *s2);
-char		**ft_stradd(char **s, char *line);
-char		**replace_line(char **s, char *line, size_t i);
-int		find_line(char **line, char *key);
-size_t		key_len(char *s);
-char		**ft_strremove(char **s, int i);
-
-// Signals
 void		catcher(void);
 void		ignore(void);
 void		reset_sig(void);
@@ -136,11 +145,8 @@ void		here_catcher(void);
 void		handler(int sig, siginfo_t *a, void *b);
 void		heredoc_signal(void);
 
-// Non interactive mode
-void		non_interactive(void);
-char		*get_input(char **argv, int argc);
-
 // Execution
+
 void		execution(t_cmd **tokens, char **env);
 void		setup_pipeline(t_cmd **tokens, char **env);
 char		*get_bin_path(char *cmd, char **env, t_pipedata *p);
@@ -149,19 +155,17 @@ bool		check_for_builtin(t_cmd **tokens);
 void		find_next_cmd_index(t_cmd **tokens, t_pipedata *p);
 void		wait_for_children(t_pipedata *p, int status);
 void		close_unused_pipes(t_pipedata *p, int index);
+
 void		init_pipes(t_pipedata *p);
 void		open_handler(t_pipedata *p, const char *path);
 void		open_file(t_cmd **tokens, t_pipedata *p, int settings);
 void		check_for_redirects(t_cmd **tokens, t_pipedata *p);
 void		setup_pipes(int in, int out, int close_in, int close_out);
 void		setup_child(t_cmd **tokens, t_pipedata *p, char **env, int i);
-int		setup_cmd_to_execute(t_cmd **tokens, t_pipedata *p);
-void		child_process(t_cmd **tokens, t_pipedata *p, char **env);
-char		*ambigous(char *s, size_t i);
+int			setup_cmd_to_execute(t_cmd **tokens, t_pipedata *p);
 
-// Cleaner
-void		clean_heredoc(void);
-t_vector	*next_check(t_vector *commands);
-void		ft_exit_child(char *s, int code);
+void		child_process(t_cmd **tokens, t_pipedata *p, char **env);
+void		cmd_help(t_vector *tokens, size_t *i, t_token *token, t_cmd *cmd);
+void		child_died(int status);
 
 #endif
