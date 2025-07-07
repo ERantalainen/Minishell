a3a752b50253561dae97e606dd8d208bacf44ded
Erantala <erantala@student.hive.fi>
2025-07-07 22:50:31 +0300

    Fixed case where there was a pipe without anything on the right side

--- src/parsing/syntax_help.c
+++ src/parsing/syntax_help.c
@@ -6,7 +6,7 @@
 /*   By: erantala <erantala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 18:40:32 by erantala          #+#    #+#             */
-/*   Updated: 2025/07/07 22:42:40 by erantala         ###   ########.fr       */
+/*   Updated: 2025/07/07 22:49:32 by erantala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,15 @@ static void output_syntax(t_cmd *cmd, t_data *data)
 			&& cmd->next != FILES)
 			data->valid = -cmd->next;
 }
-static void appen_syntax(t_cmd *cmd, t_data *data)
+static void additional_syntax(t_cmd *cmd, t_data *data)
 {
 	if (cmd->type == APPEND && cmd->next == EMPTY)
 		data->valid = -1;
 	else if (cmd->type == APPEND && cmd->next != STRING
 		&& cmd->next != FILES)
 		data->valid = -cmd->next;
+	if (cmd->type == PIPE && cmd->next == EMPTY)
+		data->valid = -1;
 }
 
 static void	check_files(t_cmd *cmd, t_cmd *next, t_data *data)
@@ -71,7 +73,7 @@ void	check_command_syntax(t_vector *commands, t_data *data)
 			return ;
 		input_syntax(cmd, data);
 		output_syntax(cmd, data);
-		appen_syntax(cmd, data);
+		additional_syntax(cmd, data);
 		if (data->valid == -1)
 			ft_fprintf(2, "%s'\n", mini_join(TOKEN, "newline"));
 		else if (data->valid != 1 && data->valid != -1)
