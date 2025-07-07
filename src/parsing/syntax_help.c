720a99e16f25135fcfc7654faa85bfe89992f774
Erantala <erantala@student.hive.fi>
2025-07-07 22:57:43 +0300

    Improved pipe syntax error

--- src/parsing/syntax_help.c
+++ src/parsing/syntax_help.c
@@ -6,7 +6,7 @@
 /*   By: erantala <erantala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 18:40:32 by erantala          #+#    #+#             */
-/*   Updated: 2025/07/07 22:49:32 by erantala         ###   ########.fr       */
+/*   Updated: 2025/07/07 22:57:14 by erantala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static void	input_syntax(t_cmd *cmd, t_data *data)
 {
 	if (cmd->type == INPUT && cmd->next == EMPTY)
-			data->valid = -1;
+			data->valid = -10;
 	else if (cmd->type == INPUT && cmd->next != FILES)
 			data->valid = -cmd->next;
 }
@@ -23,20 +23,22 @@ static void	input_syntax(t_cmd *cmd, t_data *data)
 static void output_syntax(t_cmd *cmd, t_data *data)
 {
 		if (cmd->type == OUTPUT && cmd->next == EMPTY)
-			data->valid = -1;
+			data->valid = -10;
 		else if (cmd->type == OUTPUT && cmd->next != STRING
 			&& cmd->next != FILES)
 			data->valid = -cmd->next;
 }
-static void additional_syntax(t_cmd *cmd, t_data *data)
+static void additional_syntax(t_cmd *cmd, t_data *data, size_t i)
 {
 	if (cmd->type == APPEND && cmd->next == EMPTY)
-		data->valid = -1;
+		data->valid = -10;
 	else if (cmd->type == APPEND && cmd->next != STRING
 		&& cmd->next != FILES)
 		data->valid = -cmd->next;
 	if (cmd->type == PIPE && cmd->next == EMPTY)
-		data->valid = -1;
+		data->valid = -10;
+	if (cmd->type == PIPE && i == 0)
+		er_pr(mini_join(TOKEN, "|'"), data, 2, 0);
 }
 
 static void	check_files(t_cmd *cmd, t_cmd *next, t_data *data)
@@ -73,10 +75,10 @@ void	check_command_syntax(t_vector *commands, t_data *data)
 			return ;
 		input_syntax(cmd, data);
 		output_syntax(cmd, data);
-		additional_syntax(cmd, data);
-		if (data->valid == -1)
-			ft_fprintf(2, "%s'\n", mini_join(TOKEN, "newline"));
-		else if (data->valid != 1 && data->valid != -1)
+		additional_syntax(cmd, data, i);
+		if (data->valid == -10)
+			ft_fprintf(2, "%s'\n", mini_join(TOKEN, "newline\'"));
+		else if (data->valid != 1 && data->valid != 0)
 		{
 			cmd = commands->data[i + 1];
 			ft_fprintf(2, "%s'\n", mini_join(TOKEN, cmd->str));
