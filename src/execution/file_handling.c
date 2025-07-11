55b3d1ea356d0b19330f9c6737f40a252e94cf43
julius <julius.pellinen@gmail.com>
2025-07-11 22:37:10 +0300

    norm

--- src/execution/file_handling.c
+++ src/execution/file_handling.c
@@ -3,50 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   file_handling.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: erantala <erantala@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: jpelline <jpelline@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 21:19:02 by jpelline          #+#    #+#             */
-/*   Updated: 2025/07/11 16:10:24 by erantala         ###   ########.fr       */
+/*   Updated: 2025/07/11 22:36:27 by jpelline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	handle_open_error(const char *cmd, const char *path)
+{
+	if (errno == EISDIR && ft_strchr(cmd, '\\'))
+		ft_fprintf(2, "%s: Is a directory\n", cmd);
+	else if (errno == ENOTDIR)
+		ft_fprintf(2, "%s: Not a directory\n", cmd);
+	else if (errno == EACCES)
+		ft_fprintf(2, "%s: Permission denied\n", cmd);
+	else if (errno == ENOENT && access(path, X_OK) < 0
+		&& (ft_strchr(cmd, '/')))
+	{
+		ft_fprintf(2, "%s: No such file or directory\n", cmd);
+		ft_exit_child(NULL, 127);
+	}
+}
+
 void	open_handler(t_pipedata *p, const char *path)
 {
 	t_stat	st;
 	int		fd;
+	char	*cmd;
 
-	if (!p->cmd_args[0])
+	cmd = p->cmd_args[0];
+	if (!cmd)
 		return ;
-	fd = open(p->cmd_args[0], O_RDONLY);
-	if (ft_strcmp(p->cmd_args[0], "..") == 0 || ft_strcmp(p->cmd_args[0],
-			".") == 0)
-		ft_exit_child(mini_join(p->cmd_args[0], CMD), 127);
-	if (stat(p->cmd_args[0], &st) == 0 && S_ISDIR(st.st_mode))
+	if (ft_strcmp(cmd, "..") == 0 || ft_strcmp(cmd, ".") == 0)
+		ft_exit_child(mini_join(cmd, CMD), 127);
+	if (stat(cmd, &st) == 0 && S_ISDIR(st.st_mode))
 	{
-		if (ft_strchr(p->cmd_args[0], '/'))
+		if (ft_strchr(cmd, '/'))
 		{
-			ft_fprintf(2, "%s: Is a directory\n", p->cmd_args[0]);
+			ft_fprintf(2, "%s: Is a directory\n", cmd);
 			ft_exit_child(NULL, 126);
 		}
-		ft_fprintf(2, "%s: command not found\n", p->cmd_args[0]);
+		ft_fprintf(2, "%s: command not found\n", cmd);
 		ft_exit_child(NULL, 127);
 	}
+	fd = open(cmd, O_RDONLY);
 	if (fd < 0)
-	{
-		if (errno == EISDIR && ft_strchr(p->cmd_args[0], '\\'))
-			ft_fprintf(2, "%s: Is a directory\n", p->cmd_args[0]);
-		else if (errno == ENOTDIR)
-			ft_fprintf(2, "%s: Not a directory\n", p->cmd_args[0]);
-		else if (errno == EACCES)
-			ft_fprintf(2, "%s: Permission denied\n", p->cmd_args[0]);
-		else if (errno == ENOENT && access(path, X_OK) < 0 && (ft_strchr(p->cmd_args[0], '/')))
-		{
-			ft_fprintf(2, "%s: No such file or directory\n", p->cmd_args[0]);
-			ft_exit_child(NULL, 127);
-		}
-	}
+		handle_open_error(cmd, path);
+	else
+		close(fd);
 }
 
 void	check_open_errno(const char *file)
