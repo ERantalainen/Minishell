cc096271f995f77693b52921ad1b17d562626a73
julius <julius.pellinen@gmail.com>
2025-07-16 20:43:30 +0300

    fixed echo, can you believe it

--- src/built_in/echo/echo.c
+++ src/built_in/echo/echo.c
@@ -6,7 +6,7 @@
 /*   By: jpelline <jpelline@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 18:34:01 by erantala          #+#    #+#             */
-/*   Updated: 2025/07/15 12:37:52 by jpelline         ###   ########.fr       */
+/*   Updated: 2025/07/16 20:42:32 by jpelline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 static size_t	skip_redirects_echo(t_cmd **tokens, size_t tok_i)
 {
 	if (tokens[tok_i] && (tokens[tok_i]->type == OUTPUT
-			|| tokens[tok_i]->type == APPEND))
+			|| tokens[tok_i]->type == APPEND
+			|| tokens[tok_i]->type == INPUT))
 	{
 		tok_i++;
 		if (tokens[tok_i] && (tokens[tok_i]->type == FILES
